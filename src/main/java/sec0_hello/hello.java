d86a3a393440015fc1f5d73c2bc60c5c307b76ac
a0lim-java <https://github.com/a0lim-java/summary.git>
2023-05-10 13:32:09 +0900

    feat: create hello and MyProfile

---
+++ src/main/java/sec0_hello/hello.java
@@ -0,0 +1,8 @@
+package sec0_hello;
+
+public class hello {
+    public static void main(String[] args){ // main method: 프로그램의 시작점
+        System.out.println("Hello"); // System: class / out: field / println: method
+        System.out.println("test");
+    }
+}
---
+++ src/main/java/sec1_basic/MyProfile.java
@@ -0,0 +1,17 @@
+package sec1_basic;
+
+// 기본형 변수: (메모리를 확보하고) 값을 가진다
+// ex) int i = 5;
+// 참조형 변수: 값을 참조한다
+// Book b = new Book();
+
+// 리터럴: 변수에 입력되는 값
+// ex) 5
+public class MyProfile {
+    public static void main(String[] args){
+        System.out.println("lim");
+        System.out.println("a0lim816@gmail.com");
+        System.out.println("female");
+        // println 대신 print를 사용하는 경우: /n 이 없어짐(모두 이어서 출력됨)
+    }
+}
