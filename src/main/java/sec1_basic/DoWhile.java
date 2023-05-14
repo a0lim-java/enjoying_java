29e770115234135667dd5b7c65e4308ae1426c26
unknown <https://github.com/a0lim-java/enjoying_java.git>
2023-05-14 18:26:35 +0900

    feat: create VendingMachnine and Main

--- src/main/java/sec1_basic/DoWhile.java
+++ src/main/java/sec1_basic/DoWhile.java
@@ -1,6 +1,5 @@
 package sec1_basic;
 
-
 /** 무조건 1번은 실행하게 할 때 사용
  * 변수의 초기화
  * do {
@@ -8,6 +7,7 @@ package sec1_basic;
  *     변수의 증감식;
  * }while(탈출 조건식);
  */
+
 public class DoWhile {
     public static void main(String[] args){
         int i = 1;
---
+++ src/main/java/sec2_OOP1/VendingMachineMain.java
@@ -0,0 +1,14 @@
+package sec2_OOP1;
+
+/**
+ * 클래스 메소드: static이 붙은 메소드
+ * 인스턴스를 만들지 않아도 사용 가능 <- 이미 메모리에 올라가 있음
+ *
+ * vendingMachineMain은 VendingMachine을 의존한다(VendingMachine이 있어야 사용 가능)
+ */
+
+public class VendingMachineMain {
+    public static void main(String[] args){
+        VendingMachine vm1 = new VendingMachine(); // 인스턴스 생성
+    }
+}
